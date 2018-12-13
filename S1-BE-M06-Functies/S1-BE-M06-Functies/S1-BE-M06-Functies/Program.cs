3e9d53afb486fd2892f455502c14cbb5d41782c0
ThijsvDorp <45030168+ThijsvDorp@users.noreply.github.com>
2018-12-13 13:42:17 +0100

    s3

---
+++ S1-BE-M06-Functies/S1-BE-M06-Functies/S1-BE-M06-Functies/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace S1_BE_M06_Functies
+{
+    class Program
+    {
+        private bool weekend()
+        {
+            DateTime date = DateTime.Now;
+            
+            return weekend();
+        }
+       
+        static void Main(string[] args)
+        {
+          
+            {
+                DateTime dt = DateTime.Now;
+                string dates = dt.ToString("yyyy-mm-dd");
+                Console.WriteLine(dates);
+                Console.ReadLine();
+            }
+        }
+    }
+}
