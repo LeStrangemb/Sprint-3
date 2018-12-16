18da046838c2271edadda89c1184ad2d3309d495
Thijs <Thijsjuh97@hotmail.com>
2018-12-16 14:41:12 +0100

    Opdracht 6.5

---
+++ S1-BE-M06-Functies/Opdracht5/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Opdracht5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Hoeveel lege regels wil je hebben?", regels);
+            Console.ReadLine();
+        }
+        public static int legeRegels()
+        {
+            int regels = 0;
+            while (regels < 2)
+            {
+                Console.WriteLine();
+                Console.ReadKey();
+            }
+            return legeRegels();
+        }
+    }
+}
+
